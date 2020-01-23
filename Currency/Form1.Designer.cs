2a8b50eeafea30e2938b87c4866786b9501ebd75
Sam <sam.hilland1@icloud.com>
2020-01-23 15:22:42 +0000

    Add files via upload

---
+++ Currency/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Currency
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OriginalCur = new System.Windows.Forms.TextBox();
+            this.ConvertedCur = new System.Windows.Forms.TextBox();
+            this.currencyfrom = new System.Windows.Forms.ComboBox();
+            this.currencyto = new System.Windows.Forms.ComboBox();
+            this.text_currencyfrom = new System.Windows.Forms.Label();
+            this.text_currencyto = new System.Windows.Forms.Label();
+            this.convertbut = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OriginalCur
+            // 
+            this.OriginalCur.Location = new System.Drawing.Point(12, 56);
+            this.OriginalCur.Name = "OriginalCur";
+            this.OriginalCur.Size = new System.Drawing.Size(142, 20);
+            this.OriginalCur.TabIndex = 0;
+            this.OriginalCur.Text = "0";
+            this.OriginalCur.TextChanged += new System.EventHandler(this.OriginalCur_TextChanged);
+            // 
+            // ConvertedCur
+            // 
+            this.ConvertedCur.Location = new System.Drawing.Point(182, 56);
+            this.ConvertedCur.Name = "ConvertedCur";
+            this.ConvertedCur.Size = new System.Drawing.Size(158, 20);
+            this.ConvertedCur.TabIndex = 1;
+            this.ConvertedCur.TextChanged += new System.EventHandler(this.DestiCur_TextChanged);
+            // 
+            // currencyfrom
+            // 
+            this.currencyfrom.FormattingEnabled = true;
+            this.currencyfrom.Location = new System.Drawing.Point(12, 25);
+            this.currencyfrom.Name = "currencyfrom";
+            this.currencyfrom.Size = new System.Drawing.Size(142, 21);
+            this.currencyfrom.TabIndex = 2;
+            this.currencyfrom.Text = "Select original currency";
+            this.currencyfrom.SelectedIndexChanged += new System.EventHandler(this.currencyfrom_SelectedIndexChanged);
+            // 
+            // currencyto
+            // 
+            this.currencyto.FormattingEnabled = true;
+            this.currencyto.Location = new System.Drawing.Point(182, 25);
+            this.currencyto.Name = "currencyto";
+            this.currencyto.Size = new System.Drawing.Size(158, 21);
+            this.currencyto.TabIndex = 3;
+            this.currencyto.Text = "Select destination currency";
+            // 
+            // text_currencyfrom
+            // 
+            this.text_currencyfrom.AutoSize = true;
+            this.text_currencyfrom.Location = new System.Drawing.Point(12, 9);
+            this.text_currencyfrom.Name = "text_currencyfrom";
+            this.text_currencyfrom.Size = new System.Drawing.Size(87, 13);
+            this.text_currencyfrom.TabIndex = 4;
+            this.text_currencyfrom.Text = "Original Currency";
+            // 
+            // text_currencyto
+            // 
+            this.text_currencyto.AutoSize = true;
+            this.text_currencyto.Location = new System.Drawing.Point(179, 9);
+            this.text_currencyto.Name = "text_currencyto";
+            this.text_currencyto.Size = new System.Drawing.Size(105, 13);
+            this.text_currencyto.TabIndex = 5;
+            this.text_currencyto.Text = "Destination Currency";
+            // 
+            // convertbut
+            // 
+            this.convertbut.Location = new System.Drawing.Point(130, 82);
+            this.convertbut.Name = "convertbut";
+            this.convertbut.Size = new System.Drawing.Size(75, 23);
+            this.convertbut.TabIndex = 6;
+            this.convertbut.Text = "Convert";
+            this.convertbut.UseVisualStyleBackColor = true;
+            this.convertbut.Click += new System.EventHandler(this.convertbut_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(354, 113);
+            this.Controls.Add(this.convertbut);
+            this.Controls.Add(this.text_currencyto);
+            this.Controls.Add(this.text_currencyfrom);
+            this.Controls.Add(this.currencyto);
+            this.Controls.Add(this.currencyfrom);
+            this.Controls.Add(this.ConvertedCur);
+            this.Controls.Add(this.OriginalCur);
+            this.MaximumSize = new System.Drawing.Size(370, 152);
+            this.MinimumSize = new System.Drawing.Size(370, 152);
+            this.Name = "Form1";
+            this.Text = "Currency Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox OriginalCur;
+        private System.Windows.Forms.TextBox ConvertedCur;
+        private System.Windows.Forms.ComboBox currencyfrom;
+        private System.Windows.Forms.ComboBox currencyto;
+        private System.Windows.Forms.Label text_currencyfrom;
+        private System.Windows.Forms.Label text_currencyto;
+        private System.Windows.Forms.Button convertbut;
+    }
+}
+
---
+++ Currency/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Currency
+{
+    public partial class Form1 : Form
+    {
+        double one;
+        double ConvertedCurrency;
+        double transactionFee;
+        double subtotal;
+        double totalCost;
+
+        public Form1()
+        {
+            //Currencies list
+            InitializeComponent();
+            currencyfrom.Items.Add("British Pound (GBP)");
+            currencyfrom.Items.Add("United States Dollar (USD)");
+            currencyfrom.Items.Add("Euro (EUR)");
+            currencyfrom.Items.Add("Australian Dollar (AUD)");
+            currencyfrom.Items.Add("Japanese Yen");
+        }
+
+        //below: Original Currency Combo Box
+        private void currencyfrom_SelectedIndexChanged(object sender, EventArgs e)
+
+        {
+            //If currencyfrom = Pound
+            if (currencyfrom.Text == "British Pound (GBP)")
+            {
+                currencyto.Items.Clear();
+                currencyto.Items.Add("United States Dollar (USD)");
+                currencyto.Items.Add("Euro (EUR)");
+                currencyto.Items.Add("Australian Dollar (AUD)");
+                currencyto.Items.Add("Japanese Yen");
+            }
+            //if currecnyfrom = USD
+            if (currencyfrom.Text == "United States Dollar (USD)")
+            {
+                currencyto.Items.Clear();
+                currencyto.Items.Add("British Pound (GBP)");
+                currencyto.Items.Add("Euro (EUR)");
+                currencyto.Items.Add("Australian Dollar (AUD)");
+                currencyto.Items.Add("Japanese Yen");
+            }
+            //If currencyfrom = EUR
+            if (currencyfrom.Text == "Euro (EUR)")
+            {
+                currencyto.Items.Clear();
+                currencyto.Items.Add("British Pound (GBP)");
+                currencyto.Items.Add("United States Dollar (USD)");
+                currencyto.Items.Add("Australian Dollar (AUD)");
+                currencyto.Items.Add("Japanese Yen");
+            }
+            //If currencyfrom = AUD
+            if (currencyfrom.Text == "Australian Dollar (AUD)")
+            {
+                currencyto.Items.Clear();
+                currencyto.Items.Add("British Pound (GBP)");
+                currencyto.Items.Add("United States Dollar (USD)");
+                currencyto.Items.Add("Euro (EUR)");
+                currencyto.Items.Add("Japanese Yen");
+            }
+            //If currencyfrom = Yen
+            if (currencyfrom.Text == "Japanese Yen")
+            {
+                currencyto.Items.Clear();
+                currencyto.Items.Add("British Pound (GBP)");
+                currencyto.Items.Add("United States Dollar (USD)");
+                currencyto.Items.Add("Euro (EUR)");
+                currencyto.Items.Add("Australian Dollar (AUD)");
+            }
+
+        }
+
+        private void OriginalCur_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DestiCur_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //Convert Button:
+        private void convertbut_Click(object sender, EventArgs e)
+        {
+
+            double one = Convert.ToDouble(OriginalCur.Text);
+            one = Math.Round(one, 2);
+
+            if (one < 1)
+            {
+                MessageBox.Show(" Min conversion is 1");
+                one = 0;
+            }
+
+            if (one > 2500)
+            {
+                MessageBox.Show("Maximum conversion is 2500");
+                one = 0;
+            }
+
+            //if currency from is conversion calcs
+            //From Pound:
+
+            if (currencyfrom.Text == "British Pound (GBP)")
+            {
+                if (currencyto.Text == "United States Dollar (USD)")
+                {
+                    ConvertedCurrency = Math.Round(one * 1.28, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+
+                if (currencyto.Text == "Euro (EUR)")
+                {
+                    ConvertedCurrency = Math.Round(one * 1.17, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+
+                if (currencyto.Text == "Australian Dollar (AUD)")
+                {
+                    ConvertedCurrency = Math.Round(one * 1.89, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+
+                if (currencyto.Text == "Japanese Yen")
+                {
+                    ConvertedCurrency = Math.Round(one * 139.51, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+
+                }
+            }
+            //From American Dollar
+            if (currencyfrom.Text == "United States Dollar (USD)")
+            {
+                if (currencyto.Text == "British Pound (GBP)")
+                {
+                    ConvertedCurrency = Math.Round(one * 0.78, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+
+                if (currencyto.Text == "Euro (EUR)")
+                {
+                    ConvertedCurrency = Math.Round(one * 0.91, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+
+                if (currencyto.Text == "Australian Dollar (AUD)")
+                {
+                    ConvertedCurrency = Math.Round(one * 1.48, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+
+                if (currencyto.Text == "Japanese Yen")
+                {
+                    ConvertedCurrency = Math.Round(one * 108.64, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+            }
+            //From Euro
+            if (currencyfrom.Text == "Euro (EUR)")
+            {
+                if (currencyto.Text == "British Pound (GBP)")
+                {
+                    ConvertedCurrency = Math.Round(one * 0.86, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+
+                if (currencyto.Text == "United States Dollar (USD)")
+                {
+                    ConvertedCurrency = Math.Round(one * 1.10, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+
+                if (currencyto.Text == "Australian Dollar (AUD)")
+                {
+                    ConvertedCurrency = Math.Round(one * 1.62, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+
+                if (currencyto.Text == "Japanese Yen")
+                {
+                    ConvertedCurrency = Math.Round(one * 119.44, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+
+            }
+            //From AUD
+            if (currencyfrom.Text == "Australian Dollar (AUD)")
+            {
+                if (currencyto.Text == "British Pound (GBP)")
+                {
+                    ConvertedCurrency = Math.Round(one * 0.53, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+
+                if (currencyto.Text == "United States Dollar (USD)")
+                {
+                    ConvertedCurrency = Math.Round(one * 0.86, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+
+                if (currencyto.Text == "Euro (EUR)")
+                {
+                    ConvertedCurrency = Math.Round(one * 0.62, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+
+                if (currencyto.Text == "Japanese Yen")
+                {
+                    ConvertedCurrency = Math.Round(one * 73.63, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+
+            }
+            //From Yen
+            if (currencyfrom.Text == "Japanese Yen")
+            {
+                if (currencyto.Text == "British Pound (GBP)")
+                {
+                    ConvertedCurrency = Math.Round(one * 0.0072, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+
+                if (currencyto.Text == "United States Dollar (USD)")
+                {
+                    ConvertedCurrency = Math.Round(one * 0.0082, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+
+                if (currencyto.Text == "Euro (EUR)")
+                {
+                    ConvertedCurrency = Math.Round(one * 0.0084, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+
+                if (currencyto.Text == "Australian Dollar (AUD)")
+                {
+                    ConvertedCurrency = Math.Round(one * 0.014, 2);
+                    ConvertedCur.Text = Convert.ToString((ConvertedCurrency));
+                }
+
+
+
+            }
+
+        }
+    }
+}
+
+
+
+
